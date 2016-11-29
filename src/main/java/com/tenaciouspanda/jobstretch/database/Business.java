e48d718877c266627c2de8026ca10d0909de0a47
thomaswilson78 <thomas_wilson78@mymail.eku.edu>
2016-11-29 13:18:23 -0500

    Added functionality over TG break.
    Added functionality such as searching, retrieving information, updated some things on the business class. Not complete as needed to get this uploaded ASAP.

--- src/main/java/com/tenaciouspanda/jobstretch/database/Business.java
+++ src/main/java/com/tenaciouspanda/jobstretch/database/Business.java
@@ -1,5 +1,5 @@
 //Keep, create variables and functions to keep,retrieve data
-package com.tenaciouspanda.jobstretch.database;
+package dbtest;
 
 import java.util.Date;
 import java.text.DateFormat;
@@ -7,26 +7,64 @@ import java.text.SimpleDateFormat;
 
 public class Business {
     private String busName;
+    private String busIndustry;
+    private String busWebsite;
     private String busSummary;
     private Date busFounded;
     private String[] busLocations;
-    
-    public String getBusinessName() {
+    Business(String n) {
+        busName = n;
+        DBconnection.getBusiness(this);
+    }
+    public void updateBus(String n, String i, String w, String s, Date f, String[] l) {
+        busName = n;
+        busIndustry = i;
+        busWebsite = w;
+        busSummary = s;
+        busFounded = f;
+        busLocations = l;
+        //DBconnection.updateBusiness(n,i,w,s,f,l);
+    }
+    //this update function handles date founded as a String and converts it into a Date variable.
+    public void updateBus(String n, String i, String w, String s, String f, String[] l) {
+        busName = n;
+        busSummary = s;
+        busIndustry = i;
+        busWebsite = w;
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            busFounded = df.parse(f);
+        }catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        busLocations = l;
+        //DBconnection.updateBusiness(n,i,w,s,busFounded,l);
+    }
+    public String getName() {
         return busName;
     }
-    public void setBusinessName(String b) {
-        busName=b;
+    public String getIndustry() {
+        return busIndustry;
     }
-    public String getBusinessSummary () {
+    public void setIndustry(String i) {
+        busIndustry = i;
+    }
+    public String getWebsite() {
+        return busWebsite;
+    }
+    public void setWebsite(String w) {
+        busWebsite = w;
+    }
+    public String getSummary () {
         return busSummary;
     }
-    public void setBusinessSummary(String s) {
-        busSummary=s;
+    public void setSummary(String s) {
+        busSummary = s;
     }
-    public Date getBusinessFounded() {
+    public Date getFounded() {
         return busFounded;
     }
-    public void setBusinessFounded(String s) {
+    public void setFounded(String s) {
         DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
         try {
             busFounded = df.parse(s);
@@ -34,10 +72,10 @@ public class Business {
             System.out.println(ex.getMessage());
         }
     }
-    public void setBusinessFounded(Date d) {
+    public void setFounded(Date d) {
         busFounded=d;
     }
-    public String[] getBusinessLocations() {
+    public String[] getLocations() {
         return busLocations;
     }
 }
