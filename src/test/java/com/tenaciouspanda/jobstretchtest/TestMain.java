4bb2a7af8e1dc9298625e8bf1df1ed5bfcddbe4b
smikulcik <smikulcik@gmail.com>
2016-12-01 12:37:47 -0500

    Add session tester

--- src/test/java/com/tenaciouspanda/jobstretchtest/TestMain.java
+++ src/test/java/com/tenaciouspanda/jobstretchtest/TestMain.java
@@ -12,5 +12,7 @@ package com.tenaciouspanda.jobstretchtest;
 public class TestMain {
     public static void main(String[] args) {
         (new GeocoderTester()).testAll();
+        (new SessionTester()).testAll();
+        System.out.println("All tests successful");
     }
 }
